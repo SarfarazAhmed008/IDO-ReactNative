e8cb14a825229562b31ee76eefc8e17793ffd154
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2018-10-02 21:55:42 +0600

    finalize

--- components/Home.js
+++ components/Home.js
@@ -6,6 +6,7 @@ import {
     TextInput,
     ScrollView,
     TouchableOpacity,
+    StatusBar,
 } from 'react-native';
 import Note from './Note';
 
@@ -17,13 +18,31 @@ export default class Home extends Component {
     onChangeInput = (noteText) =>{
         this.setState({noteText});
     };
+    addNote = () => {
+        const {notesList, noteText} = this.state;
+        var d = new Date();
+        if(noteText != ''){
+            notesList.push({
+                'date' : d.getDate() + "/" + (d.getMonth()+1) + "/" + d.getFullYear(),
+                'note' : noteText,
+            });
+            this.setState({noteText: ''});
+            this.setState({notesList});
+        }  
+    };
+    deleteNoteHandler = (key) => {
+        const {notesList} = this.state;
+        notesList.splice(key, 1);
+        this.setState({notesList});
+    };
     render() {
-        const {notesList, noteText} = this.state; 
-        const notes = notesList.map((key, val) => {
-
-        });
+        const {noteText, notesList} = this.state;
+        const notes = notesList.map((val, key) => (
+             <Note key={key} keyVal={key} note={val} deleteNote = {() => this.deleteNoteHandler(key)}/>
+        ));
         return (
             <View style={styles.container}>
+                <StatusBar barStyle="light-content" />
                 <View style={styles.headerContainer}>
                     <Text style={styles.headerText}>IDO - Application</Text>
                 </View>
@@ -34,13 +53,13 @@ export default class Home extends Component {
                     <TextInput 
                         autoCorrect={false}
                         placeholder = "Enter Note"
-                        placeholderTextColor= 'black'
                         underlineColorAndroid = 'transparent'
                         onChangeText= {this.onChangeInput}
                         value={noteText}
+                        style={styles.inputStyle}
                     />
                 </View>
-                <TouchableOpacity style= {styles.addButton} onPress={}>
+                <TouchableOpacity style= {styles.addButton} onPress={this.addNote}>
                     <Text style={styles.buttonText}>+</Text>
                 </TouchableOpacity>
             </View>
@@ -53,22 +72,55 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     headerContainer : {
+        backgroundColor: 'slateblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 10,
+        borderBottomColor: 'dimgrey'
 
     },
     headerText : {
-        color: 'black',
+        color: 'gainsboro',
         fontFamily: 'Roboto',
+        fontSize: 30,
+        padding: 26,
     },
     scrollStyle : {
-
+        flex: 1,
+        marginBottom: 10,
     },
     footerContainer : {
-        
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 10
+
+    },
+    inputStyle: {
+        alignSelf: 'stretch',
+        color: '#fff',
+        padding: 20,
+        backgroundColor: '#252525',
+        borderTopWidth:5,
+        borderTopColor: '#ededed',
     },
     addButton : {
+        position: 'absolute',
+        zIndex: 11,
+        right: 20,
+        bottom: 80,
+        backgroundColor: 'slateblue',
+        width: 70,
+        height: 70,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 9,
 
     },
     buttonText: {
-
+        fontSize: 40,
+        color: 'white',
     },
 });
--- components/Note.js
+++ components/Note.js
@@ -3,21 +3,51 @@ import {
     StyleSheet,
     Text,
     View,
+    TouchableOpacity,
 } from 'react-native';
 
 
 export default class Note extends Component {
     render() {
+        const {keyVal, note} = this.props;
         return (
-            <View style={styles.container}>
-                
+            <View style={styles.note} key={keyVal} >
+                <Text style={styles.noteText}>{note.note}</Text>
+                <Text style={styles.noteText}>{note.date}</Text>
+                <TouchableOpacity style={styles.delButton} onPress={this.props.deleteNote}>
+                    <Text style={styles.buttonText}>D</Text>
+                </TouchableOpacity>
             </View>
         );
       }
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex: 1,
+    note : {
+        position: 'relative',
+        padding: 20,
+        paddingRight: 100,
+        borderBottomWidth:2,
+        borderBottomColor: '#ededed',
+    },
+    noteText : {
+        paddingLeft: 20,
+        borderLeftWidth: 5,
+        borderLeftColor: 'royalblue',
+        fontSize: 17,
+    },
+    delButton : {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightcoral',
+        padding: 10,
+        top: 10,
+        bottom: 10,
+        right: 10
+
+    },
+    buttonText : {
+        color: 'fff',
     },
 });
